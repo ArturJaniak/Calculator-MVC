898f0243b9665131b7257c65682f2ee40bb6e63a
Pyro <Artur.t.janiak@gmail.com>
2021-04-23 13:15:24 +0200

    Add project files.

---
+++ mvcProjecttrue/Controllers/CalcController.cs
@@ -0,0 +1,41 @@
+﻿using mvcProjecttrue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcProjecttrue.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calc c,string calculate)
+        {
+            if (calculate == "add") 
+            {
+                c.tot = c.no1 + c.no2;
+            }
+            else if (calculate == "sub")
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            else if (calculate == "mul")
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            else if (calculate == "div")
+            {
+                c.tot = c.no1 / c.no2;
+            }
+
+            return View(c);
+        }
+    }
+}
---
+++ mvcProjecttrue/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcProjecttrue.MvcApplication" Language="C#" %>
